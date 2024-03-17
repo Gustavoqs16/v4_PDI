54086d4ee5cc5469f58dc6e2603e66b46d20d266
Gustavoqs16 <gustavo_qs16@hotmail.com>
2024-03-17 13:09:19 -0300

    feat: pdi page

--- src/app/icons/icon-v4/icon-v4.component.ts
+++ src/app/icons/icon-v4/icon-v4.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class IconV4Component implements OnInit {
   @Input() name = '';
+  @Input() class = '';
   constructor() { }
 
   ngOnInit() {}
--- src/app/restrict/pdi/pdi.page.ts
+++ src/app/restrict/pdi/pdi.page.ts
@@ -42,13 +42,67 @@ export class PdiPage implements OnInit {
       label: 'teste',
       key: false,
     },
-    {
-      label: 'teste',
-      key: false,
-    },
   ];
+
+  awards = [
+    {
+      type: 'awards',
+      url: '../../../assets/images/awards/awards.png'
+    },
+    {
+      type: 'collab',
+      url: '../../../assets/images/awards/collab.png'
+    },
+    {
+      type: 'stars',
+      url: '../../../assets/images/awards/stars.png'
+    },
+    {
+      type: 'performance',
+      url: '../../../assets/images/awards/performance.png'
+    },
+    {
+      type: 'medal-of-honor',
+      url: '../../../assets/images/awards/medal-of-honor.png'
+    },
+    {
+      type: 'account',
+      url: '../../../assets/images/awards/account.png'
+    },
+    {
+      type: 'forever',
+      url: '../../../assets/images/awards/forever.png'
+    },
+  ]
+
+  list = [
+    {
+      name: 'nome 1',
+      message: 'mensagem nova',
+      check: false
+    },
+    {
+      name: 'nome 2',
+      message: 'mensagem nova',
+      check: true
+    },
+  ]
 
   constructor() {}
 
   ngOnInit() {}
+
+  favoriteChanged(event: any) {
+    // Lógica para lidar com a mudança de estado do checkbox
+    console.log('Checkbox checked:', event.detail.checked);
+  }
+
+  getCheck(elem: any): string {
+    let checkIcon = elem.check? 'heart' : 'heart-outline';
+    return checkIcon;
+  }
+
+  toggleCheck(index: number) {
+    this.list[index].check = !this.list[index].check;
+  }
 }
