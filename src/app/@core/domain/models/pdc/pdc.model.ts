72dc0cb169e735cbe0407d79dbb52992fcd3a9ff
gustavosrossi <gustavo.rossi@outlook.com.br>
2024-04-17 00:05:06 -0300

    feat: add to new services

---
+++ src/app/@core/domain/models/pdc/pdc.model.ts
@@ -0,0 +1,18 @@
+import { BaseModel } from "../base.model";
+import { CompanyModel } from "../company/company.model";
+import { SectorModel } from "../sector/sector.model";
+import { UsersModel } from "../users/users.model";
+
+export class PdcModel extends BaseModel {
+
+    name: string;
+    image: string;
+    order: number;
+    companiesId: number;
+    company: CompanyModel;
+    sector: SectorModel;
+    sectorId: number;
+    userId: number;
+    last_user_update: UsersModel;
+
+}
